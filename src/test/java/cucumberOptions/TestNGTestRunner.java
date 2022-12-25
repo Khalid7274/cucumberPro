5f038c849237648c3387eb1e3931250862a89406
Nematullah Khalid <naimt.khalid@gmail.com>
2022-12-25 12:30:00 -0500

    second commit

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/features", tags="@PlaceOrder or @offersPage",
+@CucumberOptions(features="src/test/java/features", tags="@PlaceOrder",
 glue="stepDefinations", monochrome=true, plugin= {"pretty","html:target/CucumberReport.html", "json:target/cucumber.json",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 		"rerun:target/failed_scenarios.txt"}
